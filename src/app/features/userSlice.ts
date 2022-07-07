aef15aafc858ace72ae547042f3790eb740e84d4
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-07-07 03:54:21 -0400

    added the ability to follow and unfollow users

--- src/app/features/userSlice.ts
+++ src/app/features/userSlice.ts
@@ -4,9 +4,13 @@ import { User } from "../../util/types";
 
 type SliceState = {
   user: User | null,
+  followers: number[] | null,
+  following: number[] | null
 }
 const initialState: SliceState =  {
-  user: null
+  user: null,
+  followers: null,
+  following: null
 }
 
 const userSlice = createSlice({
@@ -15,9 +19,15 @@ const userSlice = createSlice({
   reducers: {
     setUser: (state, { payload }) => {
       state.user = payload;
+    },
+    setFollowers: (state, { payload }) => {
+      state.followers = payload;
+    },
+    setFollowing: (state, { payload }) => {
+      state.following = payload;
     }
   }
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setFollowing, setFollowers } = userSlice.actions;
 export const userReducer = userSlice.reducer;
--- src/components/pages/UserProfile.tsx
+++ src/components/pages/UserProfile.tsx
@@ -6,6 +6,7 @@ import { ImArrowLeft } from "react-icons/im";
 import { setError } from "../../app/features/errorSlice";
 import { setPosts, setReposts, setUpvotes, setDownvotes, setBookmarks } from "../../app/features/postSlice";
 import { setComments, setCommentsDownvotes, setCommentsReposts, setCommentsUpvotes } from "../../app/features/commentSlice";
+import { setFollowers, setFollowing } from "../../app/features/userSlice";
 import { useAppDispatch, useAppSelector } from "../../util/hooks";
 import { User } from "../../util/types";
 import Post from "../post/Post";
@@ -14,13 +15,18 @@ import Comment from "../comment/Comment";
 const UserProfile = () => {
   const [user, setUser] = useState<User | null>(null);
   const [numOfPosts, setNumOfPosts] = useState(0);
+  const followerList = useAppSelector(state => state.user.followers);
+  const followingList = useAppSelector(state => state.user.following);
   const loggedUser = useAppSelector(state => state.user.user);
   const posts = useAppSelector(state => state.posts.posts);
   const { userId } = useParams();
+  const [isFollowing, setIsFollowing] = useState(followingList?.includes(userId ? parseInt(userId) : 0));
+  const [isFollower, setIsFollower] = useState(followerList?.includes(userId ? parseInt(userId) : 0));
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const feed = [];
 
+  console.log(isFollowing, isFollower)
   useEffect(() => {
     Axios({
       method: "POST",
@@ -84,6 +90,65 @@ const UserProfile = () => {
     })
   }, [loggedUser, navigate, dispatch])
 
+  useEffect(() => {
+    Axios({
+      method: "POST",
+      withCredentials: true,
+      data: { user_id: loggedUser?.id },
+      url: "http://localhost:4001/user/getFollowers"
+    }).then((res) => dispatch(setFollowers(res.data)))
+      .catch((err) => {
+        dispatch(setError(err.response.data));
+        navigate("*");
+    })
+  }, [loggedUser, navigate, dispatch])
+
+  useEffect(() => {
+    Axios({
+      method: "POST",
+      withCredentials: true,
+      data: { follower_id: loggedUser?.id },
+      url: "http://localhost:4001/user/getFollowing"
+    }).then((res) => dispatch(setFollowing(res.data)))
+      .catch((err) => {
+        dispatch(setError(err.response.data));
+        navigate("*");
+    })
+  }, [loggedUser, navigate, dispatch])
+
+  const follow = () => {
+    if (isFollowing) {
+      Axios({
+        method: "POST",
+        withCredentials: true,
+        data: {
+          user_id: userId,
+          follower_id: loggedUser?.id
+        },
+        url: "http://localhost:4001/user/unfollow"
+      }).then((res) => setIsFollowing(!isFollowing))
+      .catch((err) => {
+        dispatch(setError(err.response.data));
+        navigate("*");
+      })
+    } else { 
+      Axios({
+        method: "POST",
+        withCredentials: true,
+        data: {
+          user_id: userId,
+          follower_id: loggedUser?.id
+        },
+        url: "http://localhost:4001/user/follow"
+      }).then((res) => setIsFollowing(!isFollowing))
+      .catch((err) => {
+        dispatch(setError(err.response.data));
+        navigate("*");
+      })
+    }
+  }
+    
+
   for (let i = 0; i < numOfPosts; i++) {
     const id = posts?.[i].id;
     if (typeof posts?.[i].media_content_type === "undefined") {
@@ -97,14 +162,14 @@ const UserProfile = () => {
     <div>
       <Link to="/home"><ImArrowLeft /></Link>
       <p>{user?.username}</p>
-      <p>{user?.num_tweets}</p>
+      <p>{user?.num_tweets} Posts</p>
     </div>
     <img src={user?.banner_photo} alt="Banner" />
     <section>
       <img src={user?.profile_photo} alt="Profile" />
         {loggedUser?.id === parseInt(userId ? userId : "")
           ? <button>Edit Profile</button>
-          : <button>Follow</button>
+          : <button onClick={follow}>{ isFollowing ? "Following" : "Follow" }</button>
         }
       <p>{user?.bio}</p>
       <p>Joined {user?.date_acc_created}</p>
